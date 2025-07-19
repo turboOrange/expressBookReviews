17bb4ff5ca252a2a34921793229c0ee655ce136b
gauthier.cedrick <cedrick_g@proton.me>
2025-07-18 23:50:10 -0400

    add author based search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,12 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const matches = Object.values(books).filter(b => b.author === author);
+  if (matches.length === 0) {
+    return res.status(404).json({ error: `No books found for author “${author}”` });
+  }
+  return res.status(200).json(matches);
 });
 
 // Get all books based on title
